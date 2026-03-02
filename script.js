0980c6656e8dac01a1a0c9ae7f0f749373aabf90
manishrashmika649-byte <manishrashmika649@gmail.com>
2026-03-02 16:30:53 +0530

    basic html and js for game intro page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // PLAY NOW event listner
+    document.getElementById('hero-play-btn').addEventListener('click', () => {
+        alert("System Booting... Authentication Required!");
+        
+    });
+});
